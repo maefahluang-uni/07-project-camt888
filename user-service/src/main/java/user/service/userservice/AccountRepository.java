880e63ce940047395c874aaaa912449ef963a0af
Srikarette <opkungkeke@gmail.com>
2023-09-15 17:26:04 +0700

    Add Account repository

---
+++ user-service/src/main/java/user/service/userservice/AccountRepository.java
@@ -0,0 +1,10 @@
+package user.service.userservice;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+    public List<Account> findById();
+
+}
