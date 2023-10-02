ee9339470f49ab39aaccdcaee3afb8e4871c3190
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-10-02 18:17:28 +0700

    Random

---
+++ lab-concert-domain-model/src/main/java/lab/end2end/concert/domain/Concert.java
@@ -0,0 +1,59 @@
+package lab.end2end.concert.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Concert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int number1;
+    private int number2;
+    private int number3;
+
+    public Concert() {
+        // Default constructor required by JPA
+    }
+
+    public Concert(int number1, int number2, int number3) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.number3 = number3;
+    }
+
+    // Getters and setters for id, number1, number2, and number3
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int getNumber3() {
+        return number3;
+    }
+
+    public void setNumber3(int number3) {
+        this.number3 = number3;
+    }
+}
+
