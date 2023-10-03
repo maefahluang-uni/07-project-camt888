582bb5de4e81a745307bb5b98fc6ad967afbe7db
Srikarette <opkungkeke@gmail.com>
2023-10-03 18:34:48 +0700

    Load Balancer update

--- loadbalancer-service/src/main/java/loadbalancer/service/loadbalancer/UserAccountServiceClient.java
+++ loadbalancer-service/src/main/java/loadbalancer/service/loadbalancer/UserAccountServiceClient.java
@@ -1,15 +1,18 @@
 // package loadbalancer.service.loadbalancer;
 
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
 // import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 // import org.springframework.cloud.openfeign.FeignClient;
 // import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestBody;
+
+// import user.service.userservice.Account;
 
 // @FeignClient(name = "user-account-service")
 // @LoadBalancerClient(name = "user-account-service", configuration =
 // LoadBalancerConfiguration.class)
+// @EntityScan(basePackages = "user.service.userservice")
 // public interface UserAccountServiceClient {
 
 // @PostMapping("/accounts")
